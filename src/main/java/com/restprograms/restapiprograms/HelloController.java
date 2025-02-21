bdac1ac03f353cd099c88729cd431aa122e9bf82
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-22 00:30:53 +0530

    [Shruti] Add. make rest call using post request method

--- src/main/java/com/restprograms/restapiprograms/HelloController.java
+++ src/main/java/com/restprograms/restapiprograms/HelloController.java
@@ -1,5 +1,6 @@
 package com.restprograms.restapiprograms;
 
+import com.restprograms.restapiprograms.UserDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +20,10 @@ public class HelloController {
     @GetMapping("/hello/param/{name}")
     public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/hello/post")
+    public String sayHelloPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 }
