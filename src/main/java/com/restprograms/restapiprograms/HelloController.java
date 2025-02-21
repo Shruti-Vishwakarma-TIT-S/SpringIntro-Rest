85cae198117943ac3e81112269cbabe438bf4ae0
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-22 00:12:15 +0530

    [Shruti] Add. make rest call to show hello 'name' from bridgelabz

--- src/main/java/com/restprograms/restapiprograms/HelloController.java
+++ src/main/java/com/restprograms/restapiprograms/HelloController.java
@@ -2,6 +2,7 @@ package com.restprograms.restapiprograms;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(name = "name", defaultValue = "Shruti") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
