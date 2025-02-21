6acdb35f91b8e7c64e11a1f4b30d4e12f9e2ef2d
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-22 00:01:23 +0530

    [Shruti] Add. demonstrate http methods using rest controller

---
+++ src/main/java/com/restprograms/restapiprograms/RestapiprogramsApplication.java
@@ -0,0 +1,14 @@
+package com.restprograms.restapiprograms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiprogramsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestapiprogramsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restprograms/restapiprograms/RestapiprogramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restprograms.restapiprograms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiprogramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
